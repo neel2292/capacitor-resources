0b2166093e649a6968277365730bf73c11821b12
neel <neel2292@gmail.com>
2020-06-29 21:30:56 +0800

    Update android.js

--- src/generate/platforms/splash/android.js
+++ src/generate/platforms/splash/android.js
@@ -3,73 +3,73 @@ module.exports = {
     type: 'splash',
     path: 'android/splash/',
     definitions: [{
-            name: "drawable-land-hdpi-screen/splash.png",
+            name: "drawable-land-hdpi/splash.png",
             width: 1280,
             height: 800,
             comment: "land-hdpi"
         }, // 480x320
         {
-            name: "drawable-land-ldpi-screen/splash.png",
+            name: "drawable-land-ldpi/splash.png",
             width: 1280,
             height: 800,
             comment: "land-ldpi"
         }, // 320x200
         {
-            name: "drawable-land-mdpi-screen/splash.png",
+            name: "drawable-land-mdpi/splash.png",
             width: 2176,
             height: 816,
             comment: "land-mdpi"
         }, // 800x480
         {
-            name: "drawable-land-xhdpi-screen/splash.png",
+            name: "drawable-land-xhdpi/splash.png",
             width: 1280,
             height: 800,
             comment: "land-xhdpi"
         }, // 1280x720
         {
-            name: "drawable-land-xxhdpi-screen/splash.png",
+            name: "drawable-land-xxhdpi/splash.png",
             width: 2720,
             height: 1632,
             comment: "land-xxhdpi"
         }, // 1600x960
         {
-            name: "drawable-land-xxxhdpi-screen/splash.png",
+            name: "drawable-land-xxxhdpi/splash.png",
             width: 1920,
             height: 1280,
             comment: "land-xxhdpi"
         }, // 1920x1280
         {
-            name: "drawable-port-hdpi-screen/splash.png",
+            name: "drawable-port-hdpi/splash.png",
             width: 480,
             height: 800,
             comment: "port-hdpi"
         }, // 320x480
         {
-            name: "drawable-port-ldpi-screen/splash.png",
+            name: "drawable-port-ldpi/splash.png",
             width: 200,
             height: 320,
             comment: "port-ldpi"
         }, // 200x320
         {
-            name: "drawable-port-mdpi-screen/splash.png",
+            name: "drawable-port-mdpi/splash.png",
             width: 480,
             height: 800,
             comment: "port-mdpi"
         }, // 480x800
         {
-            name: "drawable-port-xhdpi-screen/splash.png",
+            name: "drawable-port-xhdpi/splash.png",
             width: 720,
             height: 1280,
             comment: "port-xhdpi"
         }, // 720x1280
         {
-            name: "drawable-port-xxhdpi-screen/splash.png",
+            name: "drawable-port-xxhdpi/splash.png",
             width: 960,
             height: 1600,
             comment: "port-xxhdpi"
         }, // 960x1600
         {
-            name: "drawable-port-xxxhdpi-screen/splash.png",
+            name: "drawable-port-xxxhdpi/splash.png",
             width: 1280,
             height: 1920,
             comment: "port-xxhdpi"
